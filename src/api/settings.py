f2050e7ddcbd5e46880007b25af4efe492ea97ac
romain-navarro38 <romain.navarro38@gmail.com>
2023-03-24 23:55:05 +0100

    Change of parameters, by unit instead of by analysis

--- src/api/settings.py
+++ src/api/settings.py
@@ -4,15 +4,9 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 RESSOURCE_DIR = BASE_DIR / "ressource"
 SETTING_JSON = BASE_DIR / "settings.json"
-DEFAULT_SETTING = {"default": "0",
-                   "0": {"analysis": "Alpha-Bêta",
-                         "label1": "Coups 1 :",
-                         "label2": "Coups 2 :",
-                         "regex": r"[1-9]\d*"},
-                   "1": {"analysis": "Tritium",
-                         "label1": "CPMA 1 :",
-                         "label2": "CPMA 2 :",
-                         "regex": r"[1-9]?\d*((\.|,){1}\d{0,2})"}}
+DEFAULT_SETTING = {"unit_default": "Coups",
+                   "time_default": 50,
+                   "unit_list": ["Coups", "Coups/s", "Coups/min"]}
 
 
 def write_json_settings(settings: dict):
@@ -20,13 +14,6 @@ def write_json_settings(settings: dict):
 
     with open(SETTING_JSON, 'w', encoding='utf-8') as f:
         json.dump(settings, f, indent=4)
-
-
-def get_default_analysis_settings() -> dict:
-    """Returns the default analysis settings"""
-
-    content = get_settings()
-    return content[content["default"]]
 
 
 def get_settings() -> dict:
@@ -38,14 +25,16 @@ def get_settings() -> dict:
     return content
 
 
-def set_default_analysis_settings(default: str):
-    """Replaces in the settings file, the default analysis"""
+def set_default_settings(setting: str, value):
+    """Replaces in the settings file, the default setting"""
 
     content = get_settings()
-    content["default"] = default
+    content[setting] = value
     write_json_settings(content)
 
 
 if __name__ == '__main__':
     write_json_settings(DEFAULT_SETTING)
-    set_default_analysis_settings("1")
+    print(get_settings())
+    set_default_settings("time_default", 400)
+    print(get_settings())
--- src/windows/main_window.py
+++ src/windows/main_window.py
@@ -1,9 +1,11 @@
-from PySide6.QtWidgets import QWidget, QHBoxLayout, QLabel, QLineEdit, QComboBox, QFormLayout
-from PySide6.QtCore import Qt
+from functools import partial
+
+from PySide6.QtWidgets import QWidget, QLabel, QLineEdit, QComboBox, QFormLayout
 from PySide6.QtGui import QFont, QRegularExpressionValidator, QIcon, QPixmap
+from PySide6.QtCore import Qt, QSize
 
 from src.api.calculation import normalised_deviation
-from src.api.settings import get_default_analysis_settings, set_default_analysis_settings
+from src.api.settings import get_settings, set_default_settings
 
 
 # noinspection PyAttributeOutsideInit
@@ -12,6 +14,7 @@ class MainWindow(QWidget):
         super().__init__()
 
         self.setWindowTitle(" ")
+        self.setMinimumSize(QSize(220, 0))
         self.setup_ui()
 
     def setup_ui(self):
@@ -21,58 +24,59 @@ class MainWindow(QWidget):
         self.modify_widgets()
         self.create_layouts()
         self.add_widgets_to_layouts()
+        self.le_value1.setFocus()
         self.setup_connections()
 
     def create_widgets(self):
-        self.cbx_analysis = QComboBox()
+        self.cbx_unit = QComboBox()
+        self.le_time = QLineEdit()
         self.la_value1 = QLabel("")
-        self.le_shocks1 = QLineEdit()
+        self.le_value1 = QLineEdit()
         self.la_value2 = QLabel("")
-        self.le_shocks2 = QLineEdit()
+        self.le_value2 = QLineEdit()
         self.la_result = QLabel("")
 
     def modify_widgets(self):
-        self.cbx_analysis.addItem("Alpha-Bêta")
-        self.cbx_analysis.addItem("Tritium")
+        settings = get_settings()
 
-        self.display_settings()
+        for unit in settings["unit_list"]:
+            self.cbx_unit.addItem(unit)
 
-        self.le_shocks1.setAlignment(Qt.AlignCenter)
-        self.le_shocks2.setAlignment(Qt.AlignCenter)
+        self.cbx_unit.setCurrentText(settings["unit_default"])
+        self.le_time.setText(str(settings["time_default"]))
 
-        self.la_result.setAlignment(Qt.AlignCenter)
+        self.change_validator_lineedit_count()
+        validator_time = QRegularExpressionValidator()
+        validator_time.setRegularExpression(r"[1-9]\d*")
+        self.le_time.setValidator(validator_time)
+
         font = QFont()
         font.setBold(True)
         self.la_result.setFont(font)
+        self.la_result.setAlignment(Qt.AlignCenter)
 
     def create_layouts(self):
-        self.layout_time = QHBoxLayout()
         self.form_layout = QFormLayout(self)
 
     def add_widgets_to_layouts(self):
-        self.form_layout.addRow(QLabel("Analyse :"), self.cbx_analysis)
-        self.form_layout.addRow(self.la_value1, self.le_shocks1)
-        self.form_layout.addRow(self.la_value2, self.le_shocks2)
+        self.form_layout.addRow(QLabel("Temps (min) :"), self.le_time)
+        self.form_layout.addRow(QLabel("Unité :"), self.cbx_unit)
+        self.form_layout.addRow(QLabel("Comptage 1 :"), self.le_value1)
+        self.form_layout.addRow(QLabel("Comptage 2 :"), self.le_value2)
         self.form_layout.addRow(QLabel(""), self.la_result)
 
     def setup_connections(self):
-        self.cbx_analysis.currentIndexChanged.connect(self.analysis_changed)
-        self.le_shocks1.textChanged.connect(self.calculation_deviation)
-        self.le_shocks2.textChanged.connect(self.calculation_deviation)
+        self.cbx_unit.currentIndexChanged.connect(self.unit_changed)
+        self.le_time.textChanged.connect(self.time_changed)
+        self.le_value1.textChanged.connect(partial(self.check_value, self.le_value1))
+        self.le_value2.textChanged.connect(partial(self.check_value, self.le_value2))
 
-    def display_settings(self):
-        """Retrieves and displays the parameters associated with the selected analysis in the combobox"""
-        settings_of_analysis = get_default_analysis_settings()
+    def check_value(self, lineedit: QLineEdit, value: str):
+        """Adds a 0 if the user input starts with a decimal separator"""
 
-        self.cbx_analysis.setCurrentText(settings_of_analysis["analysis"])
-
-        input_validator = QRegularExpressionValidator()
-        input_validator.setRegularExpression(settings_of_analysis["regex"])
-        self.le_shocks1.setValidator(input_validator)
-        self.le_shocks2.setValidator(input_validator)
-
-        self.la_value1.setText(settings_of_analysis["label1"])
-        self.la_value2.setText(settings_of_analysis["label2"])
+        if value.startswith(".") or value.startswith(","):
+            lineedit.setText(f"0{value}")
+        self.calculation_deviation()
 
     def calculation_deviation(self):
         """If data present, calculation and display of the result of the normalized deviation"""
@@ -85,16 +89,20 @@ class MainWindow(QWidget):
         self.display_result(deviation)
 
     def data_transformation(self) -> dict:
-        """Transforms the data according to the selected analysis to be compatible
-        with the calculation of the normalized deviation"""
+        """Transforms the data according to the unit and counting time entered
+        to be compatible with the calculation of the normalized deviation"""
 
-        shocks1 = float(self.le_shocks1.text().replace(',', '.'))
-        shocks2 = float(self.le_shocks2.text().replace(',', '.'))
-        if self.cbx_analysis.currentText() == "Tritium":
-            shocks1 *= 60
-            shocks2 *= 60
+        count1 = float(self.le_value1.text().replace(',', '.'))
+        count2 = float(self.le_value2.text().replace(',', '.'))
+        time = int(self.le_time.text())
+        if self.cbx_unit.currentText() == "Coups/s":
+            count1 *= 60 * time
+            count2 *= 60 * time
+        elif self.cbx_unit.currentText() == "Coups/min":
+            count1 *= time
+            count2 *= time
 
-        return {"shocks1": shocks1, "shocks2": shocks2}
+        return {"count1": count1, "count2": count2}
 
     def display_result(self, result: float):
         """Changes the interface according to the result of the normalized deviation"""
@@ -108,20 +116,39 @@ class MainWindow(QWidget):
             self.la_result.setStyleSheet("color: green")
             self.la_result.setText("OK")
 
-    def analysis_changed(self):
-        """Changes the default analysis in the settings, deletes user entries and the result.
-        Displays the settings for the selected analysis."""
+    def change_validator_lineedit_count(self):
+        """Changes the validator of the user inputs according to the selected unit"""
 
-        set_default_analysis_settings(str(self.cbx_analysis.currentIndex()))
-        self.le_shocks1.setText("")
-        self.le_shocks2.setText("")
+        validator = QRegularExpressionValidator()
+        if self.cbx_unit.currentText() == "Coups":
+            validator.setRegularExpression(r"[1-9]\d*")
+        else:
+            validator.setRegularExpression(r"([0](\.|,)|(\.|,)|[1-9]\d*(\.|,))\d*")
+
+        self.le_value1.setValidator(validator)
+        self.le_value2.setValidator(validator)
+
+    def unit_changed(self):
+        """Changes the default unit in the settings, deletes user entries and the result"""
+
+        set_default_settings("unit_default", self.cbx_unit.currentText())
+        self.le_value1.setText("")
+        self.le_value2.setText("")
         self.la_result.setText("")
-        self.display_settings()
+        self.change_validator_lineedit_count()
+
+    def time_changed(self):
+        """Change the default time in the settings, and start the calculation"""
+
+        set_default_settings("time_default", self.le_time.text())
+        self.calculation_deviation()
 
     def verification_widget_filling(self) -> bool:
         """Checking the presence of data for the calculation"""
 
-        return bool(self.le_shocks1.text() and self.le_shocks2.text())
+        return bool(self.le_time.text()
+                    and self.le_value1.text()
+                    and self.le_value2.text())
 
 
 if __name__ == '__main__':
